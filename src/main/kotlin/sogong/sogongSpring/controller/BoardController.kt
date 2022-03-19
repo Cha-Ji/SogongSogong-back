6a63d546799f59bbc60c8bf5d81dc8f26d0f0f39
차지원 <chajiwon100785@gmail.com>
2022-03-19 16:48:12 +0900

    Refactor BoardController
    개행, 공백, 반환방식(return -> =) 수정

--- src/main/kotlin/sogong/sogongSpring/controller/BoardController.kt
+++ src/main/kotlin/sogong/sogongSpring/controller/BoardController.kt
@@ -1,6 +1,5 @@
 package sogong.sogongSpring.controller
 
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
@@ -10,90 +9,87 @@ import sogong.sogongSpring.dto.board.EntireCommentDto
 import sogong.sogongSpring.dto.board.EntirePostDto
 import sogong.sogongSpring.dto.board.ScrapLikeDto
 import sogong.sogongSpring.dto.boardedit.DeleteCommentDto
-import sogong.sogongSpring.dto.boardprint.PrintEntirePostDto
-import sogong.sogongSpring.entity.EntireCommentEntity
-import sogong.sogongSpring.entity.EntirePostEntity
 import sogong.sogongSpring.service.BoardService
 import sogong.sogongSpring.service.BoardEditService
 import sogong.sogongSpring.service.BoardPrintService
 
-
 @RestController // JSON 형태로 결과 반환해줌!
 //@RequiredArgsConstructor // final 객체를 Constructor Injection 해준다! (Autowire 역할)
 @RequestMapping("/board")
-class BoardController (var boardService: BoardService, var boardServiceEdit: BoardEditService, var boardPrintService : BoardPrintService){
+class BoardController (
+    val boardService: BoardService,
+    val boardServiceEdit: BoardEditService,
+    val boardPrintService : BoardPrintService) {
 
     @PostMapping("/post")
-    fun registPost(@RequestBody entirePostDto : EntirePostDto){
+    fun registPost(@RequestBody entirePostDto: EntirePostDto) {
         boardService.saveBoard(entirePostDto) //바로 Service로 갑니다^^
     }
 
     @PostMapping("/comment")
-    fun registComment(@RequestBody entireCommentDto : EntireCommentDto){
+    fun registComment(@RequestBody entireCommentDto: EntireCommentDto) {
         boardService.saveComment(entireCommentDto)
     }
 
     @PostMapping("/scrap-like")
-    fun registScrapLike(@RequestBody scrapLikeDto: ScrapLikeDto){
+    fun registScrapLike(@RequestBody scrapLikeDto: ScrapLikeDto) {
         boardService.saveScrapLike(scrapLikeDto)
     }
 
    @GetMapping(value = ["/post-auth", "/comment-auth"])
-    fun editAuth(@RequestParam("user-id") userId:Long, @RequestParam("post-id") postid:Long,
-                 @RequestParam("comment-id") commentid:Long?=null) : Boolean{
-        return boardServiceEdit.editAuth(EditPostCommentAuthDto(
-            postId=postid, userId=userId, commentId=commentid))
-    }
+    fun editAuth(@RequestParam("user-id") userId: Long,
+                 @RequestParam("post-id") postid: Long,
+                 @RequestParam("comment-id") commentid: Long? = null
+   ): Boolean = boardServiceEdit.editAuth(
+       EditPostCommentAuthDto(
+            postId = postid,
+            userId = userId,
+            commentId = commentid
+       )
+   )
 
     @PutMapping("/post/{postId}")
-    fun editPost(@PathVariable postId:Long, @RequestBody editPostDto:EntirePostDto){
+    fun editPost(@PathVariable postId:Long, @RequestBody editPostDto: EntirePostDto) {
         boardServiceEdit.editPost(postId, editPostDto)
     }
 
     @PutMapping("/comment/{commentId}")
-    fun editComment(@PathVariable commentId:Long, @RequestBody editCommentDto:EntireCommentDto) {
+    fun editComment(@PathVariable commentId: Long, @RequestBody editCommentDto: EntireCommentDto) {
         boardServiceEdit.editComment(commentId, editCommentDto)
     }
 
     @DeleteMapping("/comment/{commentId}")
-    fun deleteComment(@PathVariable commentId : Long, @RequestParam("post-id") postId: Long){
-        val deleteCommentDto = DeleteCommentDto(commentId, postId)
-        boardServiceEdit.deleteComment(deleteCommentDto)
+    fun deleteComment(@PathVariable commentId: Long, @RequestParam("post-id") postId: Long) {
+        boardServiceEdit.deleteComment(DeleteCommentDto(commentId, postId))
     }
 
     @DeleteMapping("/post/{postId}")
-    fun deletePost(@PathVariable postId: Long){
+    fun deletePost(@PathVariable postId: Long) {
         boardServiceEdit.deletePost(postId)
     }
 
     @GetMapping("/entire-post")
-    fun printPost(@PageableDefault(size=20, sort=["postId"], direction= Sort.Direction.DESC) pageable: Pageable) : Page<EntirePostEntity> {
-        return boardPrintService.printEntirePost(pageable)
-    }
+    fun printPost(@PageableDefault(size=20, sort=["postId"], direction= Sort.Direction.DESC) pageable: Pageable) =
+        boardPrintService.printEntirePost(pageable)
 
     @GetMapping("/one-post/{postId}")
-    fun printOnePost(@PathVariable postId:Long) : EntirePostEntity{
-        return boardPrintService.printOnePost(postId)
-    }
+    fun printOnePost(@PathVariable postId:Long) = boardPrintService.printOnePost(postId)
 
     @GetMapping("/comment")
-    fun printComment(@RequestParam("post-id") postId:Long) : MutableList<EntireCommentEntity>{
-        return boardPrintService.printComment(postId)
-    }
+    fun printComment(@RequestParam("post-id") postId:Long) = boardPrintService.printComment(postId)
+
 
     @GetMapping("/scrap-like")
-    fun printScrapLike(@RequestParam("user-id") userId:Long,
-                       @RequestParam("scrap-like") scrapLike:Boolean) : MutableList<PrintEntirePostDto>{
-        return boardPrintService.printScrapLike(userId, scrapLike)
-    }
+    fun printScrapLike(
+        @RequestParam("user-id") userId:Long,
+        @RequestParam("scrap-like") scrapLike:Boolean
+    ) = boardPrintService.printScrapLike(userId, scrapLike)
 
     @GetMapping("/hot-post")
-    fun printHotPost(@PageableDefault(size=20, sort=["postId"], direction= Sort.Direction.DESC) pageable: Pageable) : List<EntirePostEntity>{
-        return boardPrintService.printHotPost(pageable).content //content만 적용
-    }
+    fun printHotPost(@PageableDefault(size=20, sort=["postId"], direction= Sort.Direction.DESC) pageable: Pageable) =
+        boardPrintService.printHotPost(pageable).content //content만 적용
 
     @GetMapping("/best-post")
-    fun printBestPost(@PageableDefault(size=20, sort=["postId"], direction= Sort.Direction.DESC) pageable: Pageable) : Page<EntirePostEntity>{
-        return boardPrintService.printBestPost(pageable) //page 전체만 적용
-    }
+    fun printBestPost(@PageableDefault(size=20, sort=["postId"], direction= Sort.Direction.DESC) pageable: Pageable) =
+        boardPrintService.printBestPost(pageable) //page 전체만 적용
 }
