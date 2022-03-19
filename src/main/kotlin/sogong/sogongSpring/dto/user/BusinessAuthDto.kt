e8af1ce68b8cd6ad9cd5b42ad2ccaa644252312e
차지원 <chajiwon100785@gmail.com>
2022-03-19 16:57:55 +0900

    Refactor BusinessAuthDto

--- src/main/kotlin/sogong/sogongSpring/dto/user/BusinessAuthDto.kt
+++ src/main/kotlin/sogong/sogongSpring/dto/user/BusinessAuthDto.kt
@@ -3,7 +3,7 @@ package sogong.sogongSpring.dto.user
 import java.io.Serializable
 
 data class BusinessAuthDto(
-    val num : String,
-    val startDate : String,
-    val pName : String
-):Serializable
+    val num: String,
+    val startDate: String,
+    val pName: String
+) : Serializable
