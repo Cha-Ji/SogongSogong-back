40dc28a62a16c1c244356a7d91e0fdae4c9540ad
차지원 <chajiwon100785@gmail.com>
2022-03-19 16:57:32 +0900

    Refactor UserHashTagDto

--- src/main/kotlin/sogong/sogongSpring/dto/hashtag/UserHashTagDto.kt
+++ src/main/kotlin/sogong/sogongSpring/dto/hashtag/UserHashTagDto.kt
@@ -1,7 +1,9 @@
 package sogong.sogongSpring.dto.hashtag
 
+import java.io.Serializable
+
 data class UserHashTagDto(
-    val userId:Long,
-    val groupId:Long,
-    var hashName:List<String>
-):java.io.Serializable
+    val userId: Long,
+    val groupId: Long,
+    var hashName: List<String>
+) : Serializable
