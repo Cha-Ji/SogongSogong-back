446708ef0e8dee97116e64ef3e9ecbd7a1dfca6d
차지원 <chajiwon100785@gmail.com>
2022-03-19 16:53:12 +0900

    Refactor EditPostCommentAuthDto (공백 제거)

--- src/main/kotlin/sogong/sogongSpring/dto/boardedit/EditPostCommentAuthDto.kt
+++ src/main/kotlin/sogong/sogongSpring/dto/boardedit/EditPostCommentAuthDto.kt
@@ -3,7 +3,7 @@ package sogong.sogongSpring.dto.boardedit
 import java.io.Serializable
 
 data class EditPostCommentAuthDto(
-    val postId : Long,
-    val userId : Long,
-    val commentId : Long ?= null
+    val postId: Long,
+    val userId: Long,
+    val commentId: Long? = null
 ) : Serializable
