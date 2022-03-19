bfa183ddb48e69b5b9d46e6aff0a6cb8df616ba7
차지원 <chajiwon100785@gmail.com>
2022-03-19 16:52:50 +0900

    Refactor DeleteCommentDte (import Serializable)

--- src/main/kotlin/sogong/sogongSpring/dto/boardedit/DeleteCommentDto.kt
+++ src/main/kotlin/sogong/sogongSpring/dto/boardedit/DeleteCommentDto.kt
@@ -1,6 +1,8 @@
 package sogong.sogongSpring.dto.boardedit
 
+import java.io.Serializable
+
 data class DeleteCommentDto(
     val commmentId : Long,
     val postId : Long
-) : java.io.Serializable
+) : Serializable
